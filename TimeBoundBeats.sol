ef7ee6e5b4ad891398acd27f06906eee16e678a7
Kerstin <kerstin@igl-web.de>
2025-06-24 16:10:19 +0800

    make it an NFT

--- TimeBoundBeats.sol
+++ TimeBoundBeats.sol
@@ -2,89 +2,116 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-//Type Definition: How does a Title look like
-struct Title {
+struct TitleMetadata {
     string name;
     string author;
     uint256 duration;
-    address owner;
 }
 
 struct Rental {
     address renter;
-    uint256 titleIndex;
+    uint256 tokenId;
     uint256 rentedUntil;
 }
 
-contract TimeBoundBeats is Ownable {
+contract TimeBoundBeats is ERC721Enumerable, Ownable {
+    using SafeERC20 for IERC20;
+    
+    // Counter for token IDs
+    uint256 private _tokenIdCounter;
+    
+    // Mapping from token ID to title metadata
+    mapping(uint256 => TitleMetadata) public titleMetadata;
+    
+    // Mapping of author to array of token IDs
+    mapping(string => uint256[]) public authorTitles;
+    
+    // Array of active rentals
+    Rental[] public rentals;
+    
+    // Payment configuration
+    IERC20 public paymentToken;
+    uint256 public rentalFee = 12; // 1 USDC/d per title; 86.400 s/d;  1 USDC = 1000000; 1000000/86400 = 11.57 = 12
+    uint256 public platformFeeBPS = 30; // 0.3% von rentalFee
+    
+    // Events
+    event PaymentTokenUpdated(address indexed newToken);
+    event TitleMinted(uint256 indexed tokenId, string name, string author);
+    event RentalFeeUpdated(uint256 newFee);
+    event TitleRented(uint256 indexed tokenId, address indexed renter, uint256 rentedUntil);
 
-//Storage Variables
-//Array of Title named titles
-Title[] public titles;
-//Array of Rental named rentals
-Rental[] public rentals;
-
-//Mapping of author to array of title indices
-mapping (string author => uint256[]) public authorTitles;
-
-// Payment configuration
-IERC20 public paymentToken;
-uint256 public rentalFee = 12; // 1 USDC/d per title; 86.400 s/d;  1 USDC = 1000000; 1000000/86400 = 11.57 = 12
-uint256 public platformFeeBPS = 30; // 0.3% von rentalFee
-
-// Events
-event PaymentTokenUpdated(address indexed newToken);
-event RentalFeeUpdated(uint256 newFee);
-
-    //Add a title
-    function addTitle(string memory _name, string memory _author, uint256 _duration) public {
-
-        Title memory title = Title(_name, _author, _duration, msg.sender);
-        titles.push(title);
-        authorTitles[_author].push(titles.length - 1);
+    constructor() ERC721("TimeBoundBeatsNFT", "TBB") {}
+    // Override required functions by both parent contracts
     }
-
-    //Get all titles
-    function getTitles() public view returns (Title[] memory) {
-        return titles;
-    }   
-
-    //Get titles by author
-    function getTitlesByAuthor(string memory _author) public view returns (Title[] memory) {
-        uint256[] memory indices = authorTitles[_author];
-        Title[] memory _titles = new Title[](indices.length);
-        for (uint256 i = 0; i < indices.length; i++) {
-            _titles[i] = titles[indices[i]];
-        }
-        return _titles;
-    }    
-
-    //Rent a title
-    function rentTitle(uint256 _index, uint256 _rentalDuration) public {
-        Title storage title = titles[_index];
-        require(title.owner != msg.sender, "You are the owner of this title");
-        require(paymentToken != IERC20(address(0)), "Payment token not configured");
-
-        uint256 totalFee = _rentalDuration * rentalFee;
-        // calculate platform fee
-        uint256 platformFee = totalFee * platformFeeBPS / 10000;
-        // calculate titleOwner fee
-        uint256 titleOwnerFee = totalFee - platformFee;
-        // Transfer platform fee
-        paymentToken.transferFrom(msg.sender, owner(), platformFee); //TODO: Use safeTransferFrom
-        // Transfer titleOwner fee
-        paymentToken.transferFrom(msg.sender, title.owner, titleOwnerFee);
+    
+    // Mint a new title NFT
+    function mintTitle(string memory _name, string memory _author, uint256 _duration) public {
+        uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter++;
         
-        Rental memory rental = Rental(msg.sender, _index, block.timestamp + _rentalDuration);
-        rentals.push(rental);
+        _safeMint(msg.sender, tokenId);
+        
+        titleMetadata[tokenId] = TitleMetadata(_name, _author, _duration);
+        authorTitles[_author].push(tokenId);
+        
+        emit TitleMinted(tokenId, _name, _author);
     }
-
+    
+    // Get all titles owned by the caller
+    function getMyTitles() public view returns (uint256[] memory) {
+        uint256 ownedCount = balanceOf(msg.sender);
+        uint256[] memory result = new uint256[](ownedCount);
+        
+        for (uint256 i = 0; i < ownedCount; i++) {
+            result[i] = tokenOfOwnerByIndex(msg.sender, i);
+        }
+        
+        return result;
+    }
+    
+    // Get titles by author
+    function getTitlesByAuthor(string memory _author) public view returns (uint256[] memory) {
+        return authorTitles[_author];
+    }
+    
+    // Get title metadata
+    function getTitleMetadata(uint256 _tokenId) public view returns (TitleMetadata memory) {
+        require(_exists(_tokenId), "Token does not exist");
+        return titleMetadata[_tokenId];
+    }
+    
+    // Rent a title
+    function rentTitle(uint256 _tokenId, uint256 _rentalDuration) public {
+        require(_exists(_tokenId), "Token does not exist");
+        require(ownerOf(_tokenId) != msg.sender, "You are the owner of this title");
+        require(paymentToken != IERC20(address(0)), "Payment token not configured");
+        
+        uint256 totalFee = _rentalDuration * rentalFee;
+        // Calculate platform fee
+        uint256 platformFee = totalFee * platformFeeBPS / 10000;
+        // Calculate titleOwner fee
+        uint256 titleOwnerFee = totalFee - platformFee;
+        
+        // Transfer platform fee
+        paymentToken.safeTransferFrom(msg.sender, owner(), platformFee);
+        // Transfer titleOwner fee
+        paymentToken.safeTransferFrom(msg.sender, ownerOf(_tokenId), titleOwnerFee);
+        
+        Rental memory rental = Rental(msg.sender, _tokenId, block.timestamp + _rentalDuration);
+        rentals.push(rental);
+        
+        emit TitleRented(_tokenId, msg.sender, block.timestamp + _rentalDuration);
+    }
+    
     // Owner functions to configure payment settings
     function setPaymentToken(address _tokenAddress) external onlyOwner {
         require(_tokenAddress != address(0), "Invalid token address");
         paymentToken = IERC20(_tokenAddress);
+        emit PaymentTokenUpdated(_tokenAddress);
     }
 
     // rentalFee = fee per rental second per title
@@ -99,13 +126,48 @@ event RentalFeeUpdated(uint256 newFee);
         require(_fee <= 10000, "Fee must be less than or equal to 100%");
         platformFeeBPS = _fee;
     }
+    
+    function isRenterOf(address _renter, uint256 _tokenId) public view returns (bool) {
+        for (uint256 i = 0; i < rentals.length; i++) {
+            if (rentals[i].renter == _renter && 
+                rentals[i].tokenId == _tokenId && 
+                rentals[i].rentedUntil >= block.timestamp) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    // Get all active rentals for a renter
+    function getMyRentals() public view returns (Rental[] memory) {
+        uint256 count = 0;
+        for (uint256 i = 0; i < rentals.length; i++) {
+            if (rentals[i].renter == msg.sender && rentals[i].rentedUntil >= block.timestamp) {
+                count++;
+            }
+        }
         
+        Rental[] memory result = new Rental[](count);
+        uint256 resultIndex = 0;
+        for (uint256 i = 0; i < rentals.length; i++) {
+            if (rentals[i].renter == msg.sender && rentals[i].rentedUntil >= block.timestamp) {
+                result[resultIndex] = rentals[i];
+                resultIndex++;
+            }
+        }
+        
+        return result;
+    }
 
 }
 
+/* Example usage:
+1. Mint a title NFT:
+   mintTitle("Kerstins Song", "Kerstin", 180)
 
-/*
-titles[0]  = Title("Kerstins Song", "author", 10, msg.sender);j
-titles[1]  = Title("Svens Song", "author", 10, msg.sender);j
-rentals[0] = Rental(0xkerstin, 1, 1123123310);
+2. Rent a title NFT:
+   rentTitle(1, 86400) // Rent token ID 1 for 24 hours
+
+3. Check rental status:
+   isRenterOf(0xUserAddress, 1) // Check if user has rented token ID 1
 */
